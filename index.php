00bf7a80cef986bc157869b6785a7db06088c000
Dawson Richey <dawsonrichey@home.lan>
2021-08-03 08:12:10 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Customer List</title>
+    <link rel="stylesheet" href="./public/css/style.css">
+  </head>
+  <body>
+
+    <h1>Customer List</h1>
+
+
+ 
+
+  </body>
+</html>
